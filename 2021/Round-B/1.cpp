f371e81703c7e911b19b62217c304ab8c64b7df4
Yifei Sun <yifei@email.com>
2021-11-13 21:19:48 -0700

    add Kick Start 2021 templates

---
+++ 2021/Round-B/1.cpp
@@ -0,0 +1,34 @@
+//
+// C++ 17
+// Google Kick Start 2021
+// Round B Problem 1
+// Created by StepBroBD <Hi@StepBroBD.com>
+//
+
+#include <iostream>
+
+#define endl "\n"
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int total_case_number;
+    cin >> total_case_number;
+    for (size_t current_case_number = 1; current_case_number <= total_case_number; ++current_case_number) {
+        cout << "Case #" << current_case_number << ": ";
+        /** ############### **/
+        /**  Begin   Input  **/
+
+        /**   End   Input   **/
+        /** ############### **/
+        /** Begin  Solution **/
+
+        /**  End  Solution  **/
+        /** ############### **/
+        cout << endl;
+    }
+    return 0;
+}
